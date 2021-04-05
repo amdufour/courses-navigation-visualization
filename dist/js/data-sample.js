98a1a0387e9ac53fbe7a8288220538067eb38790
Anne-Marie Dufour <hello@annemariedufour.com>
2021-04-05 12:36:11 +0200

    Update .gitignore and data files

---
+++ dist/js/data-sample.js
@@ -0,0 +1,592 @@
+/*! project-name v0.0.1 | (c) 2021 YOUR NAME | MIT License | http://link-to-your-git-repo.com */
+data = [
+  {
+      "id": "1542",
+      "slug": "summarize-news-articles-with-nlp-deep-learning-and-python",
+      "title": [
+          "Summarize News Articles with NLP, Deep Learning, and Python"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "",
+      "difficulty": "INTERMEDIATE",
+      "skills_learned": "Convert an abstractive text summarization dataset to an extractive one, Train a deep learning model to perform extractive text summarization",
+      "topics": [
+          "Natural Language Processing",
+          "Deep Learning"
+      ],
+      "concepts": [
+          "Classification",
+          "Transformers"
+      ],
+      "skills": [
+          "Text manipulation"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "TensorFlow",
+          "Keras"
+      ],
+      "skills_needed": "Intermediate Python, Beginner TensorFlow/Keras, Basics of NLP, Basics of Deep Learning",
+      "value": 1
+  },
+  {
+      "id": "1458",
+      "slug": "using-deep-learning-to-predict-basketball-scores",
+      "title": [
+          "Using Deep Learning to Predict Basketball Scores"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "2020-10-06 04:00:00+00",
+      "difficulty": "INTERMEDIATE",
+      "skills_learned": "Use Pandas to standardize data, Create Neural Network with Keras, Convert trained Deep Learning network for use on the web, Convert trained Deep Learning network to h5 for use with TensorFlow.js",
+      "topics": [
+          "Deep Learning"
+      ],
+      "concepts": [
+          "Neural Networks"
+      ],
+      "skills": [
+          "Data manipulation",
+          "Web development"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "TensorFlow",
+          "Pandas",
+          "Keras"
+      ],
+      "skills_needed": "Intermediate Python, Beginner TensorFlow, Basics of Deep Learning",
+      "value": 1
+  },
+  {
+      "id": "1437",
+      "slug": "sentiment-analysis-and-natural-language-processing-for-marketing",
+      "title": [
+          "Sentiment Analysis and Natural Language Processing for Marketing"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "2020-09-28 04:00:00+00",
+      "difficulty": "ADVANCED",
+      "skills_learned": "Perform sampling from imbalanced data, Dictionary-based sentiment analysis, Analyze reviews with Deep Learning, Compare classifier performance",
+      "topics": [
+          "Natural Language Processing",
+          "Deep Learning"
+      ],
+      "concepts": [
+          "Sentiment Analysis"
+      ],
+      "skills": [
+          "Data sampling",
+          "Performance"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "scikit-learn",
+          "PyTorch"
+      ],
+      "skills_needed": "Intermediate Python, Beginner scikit-learn, Beginner PyTorch, Basics of NLP, Basics of Deep Learning",
+      "value": 1
+  },
+  {
+      "id": "1524",
+      "slug": "authorship-identification-with-text-mining-and-machine-learning",
+      "title": [
+          "Authorship Identification with Text Mining and Machine Learning"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "",
+      "difficulty": "INTERMEDIATE",
+      "skills_learned": "Extract features from text using scikit-learn and spaCy, Build a predictive classification model, Visualise authorship styles with an interactive plot, Incorporate your trained model into a user-friendly program",
+      "topics": [
+          "Machine Learning"
+      ],
+      "concepts": [
+          "Classification",
+          "Predictive models"
+      ],
+      "skills": [
+          "Data sampling",
+          "Data visualization"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "Pandas",
+          "NumPy",
+          "scikit-learn",
+          "spaCy"
+      ],
+      "skills_needed": "Beginner Python, Basics of pandas, Basics of NumPy, Basics of machine learning and scikit-learn",
+      "value": 1
+  },
+  {
+      "id": "1465",
+      "slug": "semi-supervised-deep-learning-with-gans-for-melanoma-detection",
+      "title": [
+          "Semi-Supervised Deep Learning with GANs for Melanoma Detection"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "2020-10-10 04:00:00+00",
+      "difficulty": "INTERMEDIATE",
+      "skills_learned": "Generative modeling, Transfer Learning, Image Classification with Deep CNNs, Semi-Supervised Learning with GANs",
+      "topics": [
+          "Deep Learning"
+      ],
+      "concepts": [
+          "Generative models",
+          "Semi-Supervised Learning"
+      ],
+      "skills": [
+          "Transfer Learning",
+          "Image Classification"
+      ],
+      "languages_and_libraries": [
+          "Deep CNNs",
+          "GANs",
+          "Python",
+          "NumPy",
+          "PyTorch"
+      ],
+      "skills_needed": "Intermediate Python, Intermediate NumPy, Beginner PyTorch, Basics of Deep Learning (CNNs)",
+      "value": 1
+  },
+  {
+      "id": "1545",
+      "slug": "introduction-to-deep-learning-with-tensorflow-2.0-for-car-model-classification",
+      "title": [
+          "Introduction to Deep Learning with TensorFlow 2.0 for Car Model Classification"
+      ],
+      "product_status_type": "NOT_RELEASED",
+      "published_date": "",
+      "difficulty": "BEGINNER",
+      "skills_learned": "Identify when to use classical machine learning algorithms and when to use deep learning. Build a neural network architecture and automate the search for parameters.",
+      "topics": [
+          "Deep Learning"
+      ],
+      "concepts": [
+          "Classification",
+          "Neural Networks"
+      ],
+      "skills": [
+          "Automation"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "TensorFlow",
+          "scikit-learn",
+          "OpenCV",
+          "NumPy"
+      ],
+      "skills_needed": "Intermediate Python, Beginner TensorFlow, Beginner scikit-learn, Basics of OpenCV and NumPy",
+      "value": 1
+  },
+  {
+      "id": "1538",
+      "slug": "predicting-safety-events-in-us-cities-with-machine-learning",
+      "title": [
+          "Predicting Safety Events in US Cities with Machine Learning"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "",
+      "difficulty": "BEGINNER",
+      "skills_learned": "Using machine learning to analyze and visualize location-based and time dependent data to make predictions, Interactive data visualization, Evaluating risks of bias and discrimination in production models",
+      "topics": [
+          "Machine Learning",
+          "Ethics and Social Concerns"
+      ],
+      "concepts": [
+          "Predictive models",
+          "Bias",
+          "Fairness"
+      ],
+      "skills": [
+          "Data Visualization"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "scikit-learn"
+      ],
+      "skills_needed": "Intermediate Python, Basics of scikit-learn",
+      "value": 1
+  },
+  {
+      "id": "1355",
+      "slug": "training-models-on-imbalanced-text-data",
+      "title": [
+          "Training Models on Imbalanced Text Data"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "2020-04-21 04:00:00+00",
+      "difficulty": "ADVANCED",
+      "skills_learned": "Data balance through sampling, Generate new corpus with deep learning, Deep Learning for text classification",
+      "topics": [
+          "Deep Learning"
+      ],
+      "concepts": [
+          "Classification",
+          "Basic Deep Models"
+      ],
+      "skills": [
+          "Data sampling",
+          "Text manipulation"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "TensorFlow"
+      ],
+      "skills_needed": "Intermediate Python, Intermediate TensorFlow, Intermediate NLP, Intermediate Deep Learning",
+      "value": 1
+  },
+  {
+      "id": "1546",
+      "slug": "life-expectancy-analysis-using-regression",
+      "title": [
+          "Life Expectancy Analysis Using Regression"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "",
+      "difficulty": "INTERMEDIATE",
+      "skills_learned": "Organize a data-mining project following the CRISP-DM model, Perform data cleaning and exploration, Perform regression modelling and diagnostics, Perform model selection",
+      "topics": [
+          "Machine Learning"
+      ],
+      "concepts": [
+          "Regression"
+      ],
+      "skills": [
+          "Data manipulation",
+          "Model selection"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "NumPy",
+          "Pandas"
+      ],
+      "skills_needed": "Intermediate Python, Basics of statistics (regression analysis), Basics of Numpy and Pandas",
+      "value": 1
+  },
+  {
+      "id": "1448",
+      "slug": "detecting-deepfakes-with-opencv-and-svm",
+      "title": [
+          "Detecting Deepfakes with OpenCV and SVM"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "2020-10-10 04:00:00+00",
+      "difficulty": "INTERMEDIATE",
+      "skills_learned": "Binary classification and evaluation of binary classifiers, SVM classification, Facial image processing",
+      "topics": [
+          "Machine Learning"
+      ],
+      "concepts": [
+          "Classification"
+      ],
+      "skills": [
+          "Image processing",
+          "Binary classification",
+          "SVM classification"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "scikit-learn",
+          "scikit-image",
+          "OpenCV"
+      ],
+      "skills_needed": "Intermediate Python, Beginner scikit-learn and scikit-image, Basics of OpenCV",
+      "value": 1
+  },
+  {
+      "id": "1534",
+      "slug": "deploying-deep-learning-model-on-web-and-mobile-applications-using-tensorflow",
+      "title": [
+          "Deploying Deep Learning Model on Web and Mobile Applications Using TensorFlow"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "",
+      "difficulty": "BEGINNER",
+      "skills_learned": "Train a Deep Learning model, Deploy a Deep Learning model using TensorFlow.js, Create web applications, Deploy your application using Docker",
+      "topics": [
+          "Deep Learning"
+      ],
+      "concepts": [
+          "Basic Deep Models"
+      ],
+      "skills": [
+          "Training a model",
+          "Deploying a model",
+          "Web development"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "TensorFlow",
+          "Docker"
+      ],
+      "skills_needed": "Intermediate Python, Basic Git, Basics of TensorFlow, Basic machine learning techniques, Basics of neural networks",
+      "value": 1
+  },
+  {
+      "id": "1460",
+      "slug": "art-style-transfer-using-neural-networks",
+      "title": [
+          "Art Style Transfer Using Neural Networks"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "2020-10-19 04:00:00+00",
+      "difficulty": "INTERMEDIATE",
+      "skills_learned": "Build a CNN, Image manipulation techniques, Transfer Learning",
+      "topics": [
+          "Deep Learning"
+      ],
+      "concepts": [
+          "Basic Deep Models"
+      ],
+      "skills": [
+          "Build a CNN",
+          "Image Manipulation",
+          "Transfer Learning"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "TensorFlow",
+          "Keras",
+          "Computer Vision"
+      ],
+      "skills_needed": "Intermediate Python, Beginner TensorFlow and Keras, Basics of Computer Vision, Basics of Deep Learning",
+      "value": 1
+  },
+  {
+      "id": "1364",
+      "slug": "building-domain-specific-language-models",
+      "title": [
+          "Building Domain Specific Language Models"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "2020-04-21 04:00:00+00",
+      "difficulty": "ADVANCED",
+      "skills_learned": "Data manipulation with NumPy and Pandas, Text preprocessing with NLTK, Train an RNN with PyTorch, Score and evaluate language models",
+      "topics": [
+          "Natural Language Processing"
+      ],
+      "concepts": [
+          "Classification"
+      ],
+      "skills": [
+          "Data manipulation",
+          "Text manipulation",
+          "Train an RNN",
+          "Score and evaluate language models"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "NLTK",
+          "PyTorch",
+          "TensorFlow"
+      ],
+      "skills_needed": "Intermediate Python, Intermediate NLTK, Beginner PyTorch or TensorFlow, Intermediate NLP, Basics of Deep Learning",
+      "value": 1
+  },
+  {
+      "id": "1362",
+      "slug": "decoding-data-science-job-postings-to-improve-your-resume",
+      "title": [
+          "Decoding Data Science Job Postings to Improve Your Resume"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "2020-04-14 04:00:00+00",
+      "difficulty": "INTERMEDIATE",
+      "skills_learned": "Process data with Pandas, Dataframes K-means clustering, Information retrieval with tf-dif, Text cluster visualization",
+      "topics": [
+          "Data Science Fundamentals"
+      ],
+      "concepts": [
+          "Classification"
+      ],
+      "skills": [
+          "Data manipulation",
+          "Data visualization",
+          "Clustering"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "scikit-learn",
+          "Pandas",
+          "tf-dif"
+      ],
+      "skills_needed": "Intermediate Python, Beginner scikit-learn, Basics of Pandas",
+      "value": 1
+  },
+  {
+      "id": "1356",
+      "slug": "use-machine-learning-to-detect-phishing-websites",
+      "title": [
+          "Use Machine Learning to Detect Phishing Websites"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "2020-04-14 04:00:00+00",
+      "difficulty": "BEGINNER",
+      "skills_learned": "Work on tabular datasets with Pandas and NumPy, Build a logistic regression classifier, Use Machine Learning for security",
+      "topics": [
+          "Machine Learning",
+          "Security"
+      ],
+      "concepts": [
+          "Regression",
+          "Classification"
+      ],
+      "skills": [
+          "Data manipulation"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "scikit-learn"
+      ],
+      "skills_needed": "Beginner Python, Beginner scikit-learn, Basics of Data Science",
+      "value": 1
+  },
+  {
+      "id": "1358",
+      "slug": "human-pose-estimation-with-deep-neural-networks",
+      "title": [
+          "Human Pose Estimation with Deep Neural Networks"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "2020-04-21 04:00:00+00",
+      "difficulty": "ADVANCED",
+      "skills_learned": "Object detection and keypoint detection algorithms, Transfer Learning, Training CNNs on images, Deploy human pose estimator using webcam",
+      "topics": [
+          "Deep Learning"
+      ],
+      "concepts": [
+          "Neural Networks"
+      ],
+      "skills": [
+          "Transfer Learning",
+          "Image processing"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "PyTorch",
+          "NumPy"
+      ],
+      "skills_needed": "Intermediate Python, Intermediate PyTorch, Intermediate NumPy, Basics of Deep Learning",
+      "value": 1
+  },
+  {
+      "id": "1486",
+      "slug": "how-to-think-about-automating-email",
+      "title": [
+          "How to Think about Automating Email"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "",
+      "difficulty": "BEGINNER",
+      "skills_learned": "Reading documentation for Python, Parsing a CSV file to store information in a data structure",
+      "topics": [
+          "Data Science Fundamentals"
+      ],
+      "concepts": [
+          "Classification"
+      ],
+      "skills": [
+          "Data manipulation"
+      ],
+      "languages_and_libraries": [
+          "Python"
+      ],
+      "skills_needed": "Beginner Python, Basics of Data Structures, Branching Constructs, and Looping Structures",
+      "value": 1
+  },
+  {
+      "id": "1359",
+      "slug": "training-and-deploying-an-ml-model-as-a-microservice",
+      "title": [
+          "Training and Deploying an ML Model as a Microservice"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "2020-04-21 04:00:00+00",
+      "difficulty": "BEGINNER",
+      "skills_learned": "Use Machine Learning for NLP, Train and evaluate a sentiment analysis model, Deploy a Machine Learning model, Build a website to connect to model's API",
+      "topics": [
+          "Machine Learning",
+          "Natural Language Processing"
+      ],
+      "concepts": [
+          "Classification",
+          "Sentiment Analysis"
+      ],
+      "skills": [
+          "Training a model",
+          "Web development",
+          "Deploying a model"
+      ],
+      "languages_and_libraries": [
+          "HTML",
+          "JavaScript",
+          "Python",
+          "Docker",
+          "AWS",
+          "Lambda"
+      ],
+      "skills_needed": "Beginner HTML, Javascript, and Python; Basics of Docker, Basics of AWS, Basics of Lambda, Basics of NLP",
+      "value": 1
+  },
+  {
+      "id": "1436",
+      "slug": "delivery-notes-data-entry-automation-with-python",
+      "title": [
+          "Delivery Notes Data Entry Automation With Python"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "2020-07-17 04:00:00+00",
+      "difficulty": "INTERMEDIATE",
+      "skills_learned": "Automate repetitive manual processes in Python, Extract data from PDFs, Web page resource identification, Dynamically assign values to web form fields using autogenerated JavaScript",
+      "topics": [
+          "Data Science Fundamentals"
+      ],
+      "concepts": [
+          "Basic Data Operations"
+      ],
+      "skills": [
+          "Data manipulation",
+          "Web development"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "pip",
+          "HTML",
+          "JavaScript"
+      ],
+      "skills_needed": "Intermediate Python, Basic pip, Basic browser development tools",
+      "value": 1
+  },
+  {
+      "id": "1354",
+      "slug": "discovering-disease-outbreaks-from-news-headlines",
+      "title": [
+          "Discovering Disease Outbreaks from News Headlines"
+      ],
+      "product_status_type": "ACTIVE",
+      "published_date": "2020-04-14 04:00:00+00",
+      "difficulty": "INTERMEDIATE",
+      "skills_learned": "Text extraction with Pandas, Clustering with KMeans and DBSCAN, Visualize clusters",
+      "topics": [
+          "Data Science Fundamentals"
+      ],
+      "concepts": [
+          "Basic Data Operations"
+      ],
+      "skills": [
+          "Text manipulation",
+          "Clustering",
+          "Data Visualization"
+      ],
+      "languages_and_libraries": [
+          "Python",
+          "scikit-learn",
+          "Pandas",
+          "KMeans",
+          "DBSCAN"
+      ],
+      "skills_needed": "Intermediate Python, Beginner scikit-learn, Basics of Pandas, Basics of Data Science",
+      "value": 1
+  }
+]
